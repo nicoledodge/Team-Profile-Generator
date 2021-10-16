a2976409bd79702721d05d6ca8c6af592215b0d8
nicoledodge <nicoledodge5@gmail.com>
2021-10-16 17:33:36 -0500

    initial changes

---
+++ index.js
@@ -0,0 +1,22 @@
+// create different requirer for classses- employee,engineer,manager,intern
+const inquirer = require("inquirer");
+// const fs = require("fs";
+
+
+const finalTeamArray = [];
+
+function firstPrompt() {
+    inquirer.prompt([
+        "Write the name of your team here"
+    ])
+    // .then (function(data)){
+    //     const teamName = data.teamname
+    // }
+}
+//add managers function
+
+//add team members function
+// add engineers
+// add interns
+// function compile team altogether - put HTML in this with the final team cards
+//then run the starting prompt
