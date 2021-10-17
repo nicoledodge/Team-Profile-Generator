dbdf2759413dbef3b714350de62185929708e0f8
nicoledodge <nicoledodge5@gmail.com>
2021-10-17 10:05:55 -0500

    added functions to index.js

--- index.js
+++ index.js
@@ -13,10 +13,43 @@ function firstPrompt() {
     //     const teamName = data.teamname
     // }
 }
+
 //add managers function
+function addManager() {
+    inquirer.prompt([
+        {
+            message: "What is your Manager's  name?",
+            name: "name"
+        },
+        {
+            message: "What is your Manager's  email?",
+            name: "email"
+        }
+    ]);
+}
 
 //add team members function
+function addTeamMembers() {
+    inquirer.prompt([{
+        message:
+        name
+:
+}
+])
+    ;
+}
+
 // add engineers
+function addEngineers() {
+    inquirer.prompt()
+}
+
 // add interns
+function addInterns() {
+    inquirer.prompt(
+        //ask the intern's school
+    )
+}
+
 // function compile team altogether - put HTML in this with the final team cards
 //then run the starting prompt
